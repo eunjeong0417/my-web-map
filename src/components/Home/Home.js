6a5ffadde2404c1e7800af2000ac21c756350a92
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-25 22:43:05 +0900

    google map api 불러오기

---
+++ src/components/Home/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+
+export class Map extends Component {
+    render() {
+        
+
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+    />
+  </GoogleMap>
+));
+        return (
+            <div>
+               <MapWithAMarker
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcUyqcJVBmLUG1QVUz3KveAxeR620ujgU&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
+
+            </div>
+        )
+    }
+}
+
+export default Map;
